caca6c63f2be620d37de96b4cd3e0eb0074f83fd
kasemkl <kmak979@gmail.com>
2024-06-06 13:12:38 +0300

    update code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,19 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './style/nav.css'
-import './style/slide.css'
-import './style/style.css'
-import './style/products.css'
-import NavBar from './components/NavBar';
-import SlideShow from './components/SlideShow';
-import Products from './components/Products';
-// import SideBar from './components/SideBar';
-import Sidebar from './components/Sidebar';
-import TopRated from './components/TopRated'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./style/nav.css";
+import "./style/slide.css";
+import "./style/style.css";
+import "./style/products.css";
+import NavBar from "./components/NavBar";
+import SlideShow from "./components/SlideShow";
+import Products from "./components/Products";
+import TopRated from "./components/TopRated";
 function App() {
   return (
     <div className="App">
-      {/* <SideBar/> */}
-      {/* <Sidebar/> */}
-      <NavBar/>
-      <SlideShow/>
-      <TopRated/>
-      <Products/>
+      <NavBar />
+      <SlideShow />
+      <TopRated />
+      <Products />
     </div>
   );
 }
